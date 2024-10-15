c7b2e2f9658666e13a5c1dbd4af5bbbbb19b59b7
Peter Sekan <sekan.pete@gmail.com>
2024-10-15 10:54:45 +0200

    change icon set

--- src/layouts/helpers/DynamicIcon.tsx
+++ src/layouts/helpers/DynamicIcon.tsx
@@ -4,7 +4,7 @@ import * as FaIcons from "react-icons/fa6";
 // import * as AiIcons from "react-icons/ai";
 // import * as BsIcons from "react-icons/bs";
 // import * as FiIcons from "react-icons/fi";
-// import * as Io5Icons from "react-icons/io5";
+import * as Io5Icons from "react-icons/io5";
 // import * as RiIcons from "react-icons/ri";
 // import * as TbIcons from "react-icons/tb";
 // import * as TfiIcons from "react-icons/tfi";
@@ -17,7 +17,7 @@ interface IDynamicIcon extends React.SVGProps<SVGSVGElement> {
 }
 
 const iconLibraries: { [key: string]: IconMap } = {
-  fa: FaIcons,
+  io: Io5Icons,
 };
 
 const DynamicIcon: FC<IDynamicIcon> = ({ icon, ...props }) => {
