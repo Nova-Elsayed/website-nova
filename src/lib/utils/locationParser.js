ad90427cbb185314c14a032374e54cbdb480328b
PDXIII <sekan.pete@gmail.com>
2025-07-10 10:28:50 +0200

    add throw error at location service

--- src/lib/utils/locationParser.js
+++ src/lib/utils/locationParser.js
@@ -1,10 +1,16 @@
 export const locationToAddress = async (location) => {
   const URL = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${location.lat}&lon=${location.lon}&zoom=20`;
   const reverseGeocodingUrl = URL;
+
+  
   const response = await fetch(reverseGeocodingUrl);
-  const json = await response.json();
-
-  // console.log(json);
-
-  return json.address;
+  if (!response.ok) {
+    const txt = response.text();
+    console.error(txt);
+    throw new Error("location to address failed");
+  } else {
+    
+    const json = await response.json();
+    return json.address;
+  }
 };
